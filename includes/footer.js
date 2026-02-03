57901acc1eede12ed2d9cc14f2bf75b34c35087f
Pedro <pedro@mail.com>
2026-02-03 20:04:21 +0100

    gros cerveau

---
+++ includes/footer.js
@@ -0,0 +1,9 @@
+const footer =`
+    <div class="container text-center footer m-0 mt-3 p-2">
+        <div class="row">
+            <p>Pierre-Maxime Verdier - 2026</p>
+        </div>
+    </div>
+`
+
+document.write(footer)
