8ab89051dc6da63ce5800e4eb0c02e8b8eac8492
Pedro <pedro@mail.com>
2026-02-04 08:42:28 +0100

    new navbar pro max ++

--- includes/navbar.js
+++ includes/navbar.js
@@ -1,56 +1,34 @@
 const url = window.location.pathname.split('/')
 const page = url[url.length -1]
-let navbar = `
-<nav class="navbar navbar-expand-lg sticky-top nav-underline" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
-            <img src="images/logo.png" width="30" height="30" class="d-inline-block align-text-center"/>
-            Ratayzyk
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
 
-                <li class="nav-item">
-                    <a class="nav-link`
-                    if(page == "index.html"){navbar+=" active"}
-                    navbar +=
-                    `" href="index.html">Hauts</a>
-                </li>
+async function addNav() {
+    const resp = await fetch("includes/navbar.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("afterbegin", html);
+}
 
-                <li class="nav-item">
-                    <a class="nav-link`
-                    if(page == "pantalons.html"){navbar+=" active"}
-                    navbar +=
-                    `" href="pantalons.html">Pantalons</a>
-                </li>
+document.addEventListener("DOMContentLoaded", async function() {//quand la page est chargée
+    await addNav()
+    let element
+    switch(page){
+        case "index.html" :
+            element="hauts"
+            break
+        case "pantalons.html":
+            element="pantalons"
+            break
+        case "vestes.html":
+            element="vestes"
+            break
+        case "robes.html":
+            element = "robes"
+            break
+        case "manteaux.html":
+            element="manteaux"
+            break
+        default:
+            element="hauts"
 
-                <li class="nav-item">
-                    <a class="nav-link`
-                    if(page == "vestes.html"){navbar+=" active"}
-                    navbar +=
-                    `" href="vestes.html">Vestes</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link`
-                    if(page == "manteaux.html"){navbar+=" active"}
-                    navbar +=
-                    `" href="manteaux.html">Manteau</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link`
-                    if(page == "robes.html"){navbar+=" active"}
-                    navbar +=
-                    `" href="robes.html">Robes</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
-`
-document.write(navbar)
+    }
+    document.getElementById(element).classList.add("active")
+})
